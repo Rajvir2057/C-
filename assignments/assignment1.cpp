e7f38e1c73a525ea8c50f640a266fa4add04882f
Rajvir <rajvirkaur2057@gmail.com>
2025-05-31 02:22:59 -0700

    addedwork

---
+++ assignments/assignment1.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <random>
+// 20 sticks game.. || kaur rajvir 816815
+
+int main(){
+    // declaring..
+    int menu;
+    // menu for the game..
+    std :: cout << "WELCOME TO THE GAME!" << '/n';
+
+    switch (menu)
+    {
+    case 1:
+        std :: cout << "1. play game";
+        break;
+
+    case 2:
+        std :: cout << "2. show rules";
+        break;
+
+    case 3:
+        std :: cout << "3. exit";
+        break;    
+    default:
+        break;
+    }
+
+    void display_rules(){
+        std :: cout << "nothing"
+    }
+
+    int getcomputerinput(){
+        std :: cout << "nothing"       
+    }
+    int userinput(){
+        std :: cout << "nothing"       
+    }
+
+    bool playagain(){
+        std :: cout << "nothing"
+    }
+
+    // 2 loops are required here.. one on keep playing till someone loses..
+    // play again needs a loop.. random will be used to pick who well plasy first..
+
+}
---
+++ lab2/haircut.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+/*
+Kaur Rajvir 
+816815*/
+// creating a haircut game using basic if and else statements.
+using namespace std;
+
+int main() {
+    // declaring the options.
+    std::string gender;
+    std::string answer;
+    std::string answer2;
+    std::string superhero;
+    std::string super_villian;
+    std::string steak;
+    std::string sushi;
+    std::string anime;
+    std::string sitcom;
+
+    // Asking using cout.. character output
+    std::cout << "What is your gender? Male or Female?" << '\n';
+    std::cin >> gender;
+
+    // using if statements
+    if (gender == "Male" || gender == "male") {
+        // asking another question
+        std::cout << "Are you a super hero or super villain?" << '\n';
+        std::cin >> answer;
+
+        if (answer == "superhero") {
+            std::cout << "last question, Do like steak or sushi?" << '\n';
+            std::cin >> answer2; 
+            // last question with answer..
+            if (answer2 == "sushi") {
+                std::cout << "You should get a flattop :)" << '\n';
+            } else if (answer2 == "steak") {
+                std::cout << "You should get a flattop :)" << '\n';
+            }
+        } else if (answer == "supervillain") {
+            std::cout << "You should get a Mohawk." << '\n';
+        }
+
+    } else if (gender == "Female" || gender == "female") {
+        // asking another question
+        std::cout << "Are you a super hero or super villain?" << '\n';
+        std::cin >> answer;
+
+        if (answer == "superhero") {
+            std::cout << "last question, Do like anime or sitcom?" << '\n';
+            std::cin >> answer2; 
+            // last question with answer..
+            if (answer2 == "anime") {
+                std::cout << "You should get bangs :)" << '\n';
+            } else if (answer2 == "sitcom") {
+                std::cout << "You should get bobmale :)" << '\n';
+            }
+        } else if (answer == "supervillain") {
+            std::cout << "You should get Mohawk." << '\n';
+        }
+    }
+
+    return 0;
+}
